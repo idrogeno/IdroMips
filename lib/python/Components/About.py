1f599cc402ce129125b6699cae37027ed34a2335
idrogeno <xentrix@hotmail.de>
2016-05-10 12:39:44 +0200

    Update

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,18 +1,38 @@
-from boxbranding import getBoxType, getImageVersion
+# -*- coding: utf-8 -*-
+import struct, socket, fcntl, sys, os, time
 from sys import modules
-import socket, fcntl, struct, time, os
+from Tools.HardwareInfo import HardwareInfo
+
+from boxbranding import getBoxType
 
 def getVersionString():
-	return getImageVersion()
+	return getImageVersionString()
+
+def getImageVersionString():
+	try:
+		if os.path.isfile('/var/lib/opkg/status'):
+			st = os.stat('/var/lib/opkg/status')
+		else:
+			st = os.stat('/usr/lib/ipkg/status')
+		tm = time.localtime(st.st_mtime)
+		if tm.tm_year >= 2011:
+			return time.strftime("%Y-%m-%d %H:%M:%S", tm)
+	except:
+		pass
+	return _("unavailable")
 
 def getFlashDateString():
 	try:
-		return time.strftime(_("%Y-%m-%d"), time.localtime(os.stat("/boot").st_ctime))
+		return time.strftime(_("%Y-%m-%d %H:%M"), time.localtime(os.stat("/boot").st_ctime))
 	except:
 		return _("unknown")
 
 def getEnigmaVersionString():
-	return getImageVersion()
+	from boxbranding import getImageVersion
+	enigma_version = getImageVersion()
+	if '-(no branch)' in enigma_version:
+		enigma_version = enigma_version [:-12]
+	return enigma_version
 
 def getGStreamerVersionString():
 	import enigma
@@ -20,58 +40,21 @@ def getGStreamerVersionString():
 
 def getKernelVersionString():
 	try:
-		f = open("/proc/version","r")
-		kernelversion = f.read().split(' ', 4)[2].split('-',2)[0]
-		f.close()
-		return kernelversion
+		return open("/proc/version","r").read().split(' ', 4)[2].split('-',2)[0]
 	except:
 		return _("unknown")
-	
-def getModelString():
-	try:
-		file = open("/proc/stb/info/boxtype", "r")
-		model = file.readline().strip()
-		file.close()
-		return model
-	except IOError:
-		return "unknown"
 
 def getChipSetString():
-	if getBoxType() in ('dm7080','dm820'):
-		return "7435"
-	else:
-		try:
-			f = open('/proc/stb/info/chipset', 'r')
-			chipset = f.read()
-			f.close()
-			return str(chipset.lower().replace('\n','').replace('bcm','').replace('brcm','').replace('sti',''))
-		except IOError:
-			return "unavailable"
-
-def getCPUSpeedString():
-	if getBoxType() in ('vusolo4k'):
-		return "1,5 GHz"
-	else:
-		try:
-			file = open('/proc/cpuinfo', 'r')
-			lines = file.readlines()
-			for x in lines:
-				splitted = x.split(': ')
-				if len(splitted) > 1:
-					splitted[1] = splitted[1].replace('\n','')
-					if splitted[0].startswith("cpu MHz"):
-						mhz = float(splitted[1].split(' ')[0])
-						if mhz and mhz >= 1000:
-							mhz = "%s GHz" % str(round(mhz/1000,1))
-						else:
-							mhz = "%s MHz" % str(round(mhz,1))
-			file.close()
-			return mhz
-		except IOError:
-			return "unavailable"
+	try:
+		f = open('/proc/stb/info/chipset', 'r')
+		chipset = f.read()
+		f.close()
+		return str(chipset.lower().replace('\n','').replace('bcm','BCM').replace('brcm','BRCM').replace('sti',''))
+	except IOError:
+		return "unavailable"
 
 def getCPUString():
-	if getBoxType() in ('xc7362', 'vusolo4k'):
+	if getBoxType() in ('xc7362'):
 		return "Broadcom"
 	else:
 		try:
@@ -109,6 +92,53 @@ def getCpuCoresString():
 	except IOError:
 		return "unavailable"
 
+def getHardwareTypeString():
+	return HardwareInfo().get_device_string()
+
+def getImageTypeString():
+	try:
+		return open("/etc/issue").readlines()[-2].capitalize().strip()[:-6]
+	except:
+		return _("undefined")
+
+def getCPUInfoString():
+	try:
+		cpu_count = 0
+		for line in open("/proc/cpuinfo").readlines():
+			line = [x.strip() for x in line.strip().split(":")]
+			if line[0] == "system type":
+				processor = line[1].split()[0]
+			if line[0] == "cpu MHz":
+				cpu_speed = "%1.0f" % float(line[1])
+				cpu_count += 1
+		if os.path.isfile('/proc/stb/fp/temp_sensor_avs'):
+			temperature = open("/proc/stb/fp/temp_sensor_avs").readline().replace('\n','')
+			return "%s %s MHz (%s) %s°C" % (processor, cpu_speed, ngettext("%d core", "%d cores", cpu_count) % cpu_count, temperature)
+		#return "%s %s MHz (%s)" % (processor, cpu_speed, ngettext("%d core", "%d cores", cpu_count) % cpu_count)
+		return "%s %s MHz" % (processor, cpu_speed)
+	except:
+		return _("undefined")
+
+def getCPUSpeedString():
+	mhz = "unavailable"
+	try:
+		file = open('/proc/cpuinfo', 'r')
+		lines = file.readlines()
+		for x in lines:
+			splitted = x.split(': ')
+			if len(splitted) > 1:
+				splitted[1] = splitted[1].replace('\n','')
+				if splitted[0].startswith("cpu MHz"):
+					mhz = float(splitted[1].split(' ')[0])
+					if mhz and mhz >= 1000:
+						mhz = "%s GHz" % str(round(mhz/1000,1))
+					else:
+						mhz = "%s MHz" % str(round(mhz,1))
+		file.close()
+		return mhz
+	except IOError:
+		return "unavailable"
+
 def _ifinfo(sock, addr, ifname):
 	iface = struct.pack('256s', ifname[:15])
 	info  = fcntl.ioctl(sock.fileno(), addr, iface)
@@ -127,9 +157,12 @@ def getIfConfig(ifname):
 	infos['hwaddr']  = 0x8927 # SIOCSIFHWADDR
 	infos['netmask'] = 0x891b # SIOCGIFNETMASK
 	try:
+		print "in TRYYYYYYY", ifname
 		for k,v in infos.items():
+			print infos.items()
 			ifreq[k] = _ifinfo(sock, v, ifname)
 	except:
+		print "IN EXCEEEEEEEEPT", ifname
 		pass
 	sock.close()
 	return ifreq
@@ -143,6 +176,14 @@ def getIfTransferredData(ifname):
 			f.close()
 			return rx_bytes, tx_bytes
 
+def getDriverInstalledDate():
+	try:
+		from glob import glob
+		driver = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/*-dvb-modules-*.control")[0], "r") if x.startswith("Version:")][0]
+		return  "%s-%s-%s" % (driver[:4], driver[4:6], driver[6:])
+	except:
+		return _("unknown")
+
 def getPythonVersionString():
 	try:
 		import commands
@@ -152,4 +193,4 @@ def getPythonVersionString():
 		return _("unknown")
 
 # For modules that do "from About import about"
-about = modules[__name__]
+about = sys.modules[__name__]
--- lib/python/Components/Element.py
+++ lib/python/Components/Element.py
@@ -17,11 +17,11 @@ def cached(f):
 	return wrapper
 
 class ElementError(Exception):
-	def __init__(self, message):
-		self.msg = message
+    def __init__(self, message):
+        self.msg = message
 
-	def __str__(self):
-		return self.msg
+    def __str__(self):
+        return self.msg
 
 class Element(object):
 	CHANGED_DEFAULT = 0   # initial "pull" state
@@ -101,7 +101,8 @@ class Element(object):
 	suspended = property(lambda self: self.__suspended, setSuspend)
 
 	def checkSuspend(self):
-		self.suspended = reduce(lambda x, y: x and y.__suspended, self.downstream_elements, True)
+		if self.downstream_elements:
+			self.suspended = reduce(lambda x, y: x and y.__suspended, self.downstream_elements, True)
 
 	def doSuspend(self, suspend):
 		pass
